88fbe0c74e56593e15439033f2f3e8276e60701a
JayamShrivastav <jayam.shrivastav@radionics.org.in>
2024-02-20 23:46:15 +0530

    firebase setup

--- app/src/main/java/com/example/internshalatask/LoginScreenFragment.kt
+++ app/src/main/java/com/example/internshalatask/LoginScreenFragment.kt
@@ -5,22 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.internshalatask.databinding.FragmentLoginScreenBinding
 
 
 class LoginScreenFragment : Fragment() {
+    private lateinit var binding : FragmentLoginScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_screen, container, false)
+    ): View {
+        binding  = FragmentLoginScreenBinding.inflate(inflater)
+        return binding.root
     }
 
     companion object {
--- app/src/main/java/com/example/internshalatask/MainActivity.kt
+++ app/src/main/java/com/example/internshalatask/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.internshalatask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.internshalatask.databinding.FragmentLoginScreenBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : FragmentLoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = FragmentLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.container, LoginScreenFragment()).addToBackStack(null).commit()
     }
 }
